cd8de58823ea39672ade22b51d25e47cf63364c6
leoli <leoli@yeahka.com>
2024-10-27 14:23:29 +0800

    deploy: 移除拦截器，改造 jar 包打包名称，方便部署展示

--- course-server/common/common-util/src/main/java/cn/edu/whut/springbear/course/common/util/interceptor/UserLoginInterceptor.java
+++ course-server/common/common-util/src/main/java/cn/edu/whut/springbear/course/common/util/interceptor/UserLoginInterceptor.java
@@ -1,6 +1,5 @@
 package cn.edu.whut.springbear.course.common.util.interceptor;
 
-import cn.edu.whut.springbear.course.common.util.alogrithm.JwtHelper;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,14 +16,14 @@ public class UserLoginInterceptor implements HandlerInterceptor {
          * 从请求头中获取用户登录后根据用户 uid 和 nickname 生成的 token，
          * 详见：cn.edu.whut.springbear.course.service.user.service.impl.WeChatServiceImpl.wechatUserRegister
          */
-        String token = request.getHeader("token");
-        if (token == null) {
-            throw new RuntimeException("[user login interceptor] null token");
-        }
-        // 从 token 字符串中读取用户 ID signKey
-        String signKey = "course-backend";
-        Object userId = JwtHelper.get(token, "uid", signKey);
-        AuthContextHolder.setUserId(Long.parseLong(String.valueOf(userId)));
+//        String token = request.getHeader("token");
+//        if (token == null) {
+//            throw new RuntimeException("[user login interceptor] null token");
+//        }
+//        // 从 token 字符串中读取用户 ID signKey
+//        String signKey = "course-backend";
+//        Object userId = JwtHelper.get(token, "uid", signKey);
+        AuthContextHolder.setUserId(Long.parseLong("34"));
         return true;
     }
 }
